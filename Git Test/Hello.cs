3b125aa1a8d13285f8536bba439af5240f9d1143
1102137103 <1102137103@gm.kuas.edu.tw>
2016-03-03 15:01:50 +0800

    add Hello.cs

---
+++ Git Test/Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Git_Test
+{
+    public class Hello
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Hi");
+            System.Console.ReadLine();
+        }
+    }
+}
